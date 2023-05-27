34319a87fefc7b31f64f08e6fb022544fd36f85f
Weather-Forecast <2263704336wf@gmail.com>
2023-05-27 19:55:36 +0800

    complete main logic of RandomTree_RI

--- RandomForest/randomtree.cpp
+++ RandomForest/randomtree.cpp
@@ -4,7 +4,11 @@
 #include <iostream>
 #include <numeric>
 
+#include "dataloader.h"
+
 using namespace std;
+
+/*====================================  RandomForest_base  ============================================*/
 
 /*
 * node of random decision tree
@@ -12,21 +16,30 @@ using namespace std;
 struct Node {
 	bool isLeaf;	// True if it is a leaf node, False if not
 	string attr;	// for internal node: record attr; for leaf node: record result
-	map<string, Node> branch_map;	// record branches
+	map<Field, Node> branch_map;	// record branches
 };
 
-struct RootNode {
-	Node* node;
+struct IG_res {
+	float IG;
+	int index;
+};
+
+struct Group_res {
+	vector<vector<vector<Field>>> X_res;
+	vector<vector<string>> y_res;
+	vector<Field> values;
 };
 
 class RandomTree_base {
 public:
-	RootNode root;
+	Node root;
 	int maxDep;
 	int numFea;
 
 	RandomTree_base(int maxdep, int numfea);
-	virtual void build(vector<vector<string>> X, vector<string> y) = 0;
+	vector<IG_res> Gini(vector<vector<Field>> X, vector<int> columns);
+	Group_res GroupData(vector<vector<Field>> X, int fea);
+	virtual void build(vector<vector<Field>> X, vector<string> y) = 0;
 };
 
 RandomTree_base::RandomTree_base(int maxdep, int numfea) {
@@ -34,14 +47,26 @@ RandomTree_base::RandomTree_base(int maxdep, int numfea) {
 	this->numFea = numfea;
 }
 
+vector<IG_res> RandomTree_base::Gini(vector<vector<Field>> X, vector<int> columns) {
+
+}
+
+Group_res RandomTree_base::GroupData(vector<vector<Field>> X, int fea) {
+
+}
+
+/*====================================  RandomForest_RI  ==============================================*/
+
 // subclass: Used for randomforest-RI. See "Random Forest" by Leo Breiman, 2001
 class RandomTree_RI : public RandomTree_base {
 public:
 	RandomTree_RI(int maxdep, int numfea) : RandomTree_base(maxdep, numfea) {};
-	void build(vector<vector<string>> X, vector<string>y);
+	void build(vector<vector<Field>> X, vector<string> y);
+	void split(vector<vector<Field>> X, vector<string> y, Node currNode, int depth);
+	string decideRes(vector<string> y);
 };
 
-void RandomTree_RI::build(vector<vector<string>> X, vector<string> y) {
+void RandomTree_RI::build(vector<vector<Field>> X, vector<string> y) {
 	int columns = X[0].size();
 	// do some input checks
 	if (columns < this->numFea) {
@@ -53,8 +78,30 @@ void RandomTree_RI::build(vector<vector<string>> X, vector<string> y) {
 		cout << "ERROR in tree building: " << "X and y size donot match\n";
 		exit(1);
 	}
+	split(X, y, this->root, 0);
+	return;
+}
+
+string RandomTree_RI::decideRes(vector<string> y) {
+
+}
+
+/* in each node we choose feature form a randomly choosed feature group, using criteria in CART
+* 
+* REFERENCE: 1. "Classification and Regression Trees", by L. Breiman et.al
+*			2. "Random Forest", by Leo Breiman
+*/
+void RandomTree_RI::split(vector<vector<Field>> X, vector<string> y, Node currNode, int depth) {
+	// TODO: stop condition
+	if (depth >= this->maxDep) {
+		Node leafNode;
+		leafNode.isLeaf = true;
+		leafNode.attr = decideRes(y);
+		return;
+	}
 
 	// randomly select features: Knuth Durstenfeld Shuffle Alg.
+	int columns = X[0].size();
 	vector<int> indexes(columns);
 	vector<int> selected(this->numFea);
 	iota(indexes.begin(), indexes.end(), 1);
@@ -63,4 +110,22 @@ void RandomTree_RI::build(vector<vector<string>> X, vector<string> y) {
 		swap(indexes[j], indexes[columns - i - 1]);
 		selected[i] = indexes[columns - i - 1];
 	}
+
+	// do split using Gini criterion
+	vector<IG_res> info_gain = Gini(X, selected);
+	// TODO: select best feature, save in (int)fea_sel
+	int fea_sel;
+
+	currNode.attr = fea_sel;
+	currNode.isLeaf = false;
+	Group_res grouped = GroupData(X, fea_sel);
+	vector<vector<vector<Field>>> X_grouped = grouped.X_res;
+	vector<vector<string>> y_grouped = grouped.y_res;
+	vector<Field> values = grouped.values;
+	for (int i = 0; i < X_grouped.size(); i++) {
+		Node newNode;
+		currNode.branch_map.insert(pair<Field, Node>(values[i], newNode));
+		split(X_grouped[i], y_grouped[i], newNode, depth + 1);
+	}
+	return;
 }
